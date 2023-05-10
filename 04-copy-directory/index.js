c565f3417a60e1dcb37dee5baab139d328f630d3
Anna <green.abrikos.73@gmail.com>
2023-05-11 01:23:30 +0300

    feat: add solution to the four tast

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const path = require('path');
+const fs = require('fs');
 
+const { stdin, stdout } = process;
+const folderToCopy = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+
+fs.mkdir(targetDir, { recursive: true }, (err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('Directory created successfully!');
+    fs.readdir(folderToCopy, (err, files) => {
+      if (err) throw err;
+      files.forEach((file) => {
+        fs.copyFile(path.join(folderToCopy, file), path.join(targetDir, file), (err) => {
+          if (err) throw err;
+        })
+      })
+      console.log("Files successfully copied");
+    })
+  }
+})
